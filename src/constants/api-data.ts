62d2261f01042a5b9e26c154d2de7ce882fd2569
Mac1618 <ma46099@gmail.com>
2024-09-06 21:56:10 +0800

    feat: Developed the footer UI with responsive design

---
+++ src/constants/api-data.ts
@@ -0,0 +1,200 @@
+export const response = [
+  {
+      "Sell Properties": {
+          "items": [
+              {
+                  "1": {
+                      "items": [],
+                      "details": {
+                          "url": "/selling-property-singapore/homer-property-valuation-tool",
+                          "title": "Homer Valuation Tool",
+                          "current_url": "https://ohmyhome.com/en-sg/property-valuation/",
+                          "is_published": true
+                      }
+                  },
+                  "2": {
+                      "items": [],
+                      "details": {
+                          "url": "/selling-property-singapore/homer-profit-calculator",
+                          "title": "Profit Calulator",
+                          "current_url": null,
+                          "is_published": false
+                      }
+                  },
+                  "3": {
+                      "items": [],
+                      "details": {
+                          "url": "/selling-property-singapore/homer-cash-proceedings-calculator",
+                          "title": "Cash Proceedings Calculator",
+                          "current_url": "https://homer.ohmyhome.com/calculator/cash-proceeds",
+                          "is_published": true
+                      }
+                  },
+                  "4": {
+                      "items": [],
+                      "details": {
+                          "url": "/selling-property-singapore/agent-finder",
+                          "title": "Agent Finder",
+                          "current_url": "https://ohmyhome.com/en-sg/singapore-property-agent",
+                          "is_published": true
+                      }
+                  },
+                  "5": {
+                      "items": [
+                          {
+                              "url": "/selling-property-singapore/sell-hdb",
+                              "title": "Sell HDB",
+                              "current_url": null,
+                              "is_published": true
+                          }
+                      ],
+                      "details": {
+                          "title": "HDB"
+                      }
+                  }
+              }
+          ]
+      }
+  },
+  {
+      "items": [
+          {
+              "1": {
+                  "items": [
+                      {
+                          "Resale HDB": {
+                              "items": [
+                                  {
+                                      "url": "/property-listing/resale-hdb",
+                                      "title": "Resale HDB Listing (Property Listing)",
+                                      "current_url": null,
+                                      "is_published": false
+                                  }
+                              ]
+                          }
+                      }
+                  ],
+                  "details": {
+                      "title": "Properties for Sale"
+                  }
+              }
+          }
+      ]
+  },
+  {
+      "Rent Properties": {
+          "items": [
+              {
+                  "For Landlords": {
+                      "items": [
+                          {
+                              "url": null,
+                              "title": "List Property for Free",
+                              "current_url": null,
+                              "is_published": false
+                          }
+                      ]
+                  }
+              }
+          ]
+      }
+  },
+  {
+      "New Launches": {
+          "items": {
+              "The Hill @ One-North": {
+                  "items": [],
+                  "details": {
+                      "url": null,
+                      "current_url": null,
+                      "is_published": false
+                  }
+              }
+          }
+      }
+  },
+  {
+      "Homer Tools": {
+          "items": [
+              {
+                  "Seller Tools": {
+                      "items": [
+                          {
+                              "url": "/property-seller-tools/homer-valuation-calculator",
+                              "title": "Homer Valuation Calculator",
+                              "current_url": "https://ohmyhome.com/en-sg/property-valuation/",
+                              "is_published": true
+                          }
+                      ]
+                  }
+              }
+          ]
+      }
+  },
+  {
+      "Property Finance": {
+          "items": [
+              {
+                  "Home Loans": {
+                      "items": [
+                          {
+                              "url": "/property-finanace/home-loans/bank-of-china",
+                              "title": "Bank of China Home Loan",
+                              "current_url": null,
+                              "is_published": false
+                          }
+                      ]
+                  }
+              }
+          ]
+      }
+  },
+  {
+      "Property Directory": {
+          "items": [
+              {
+                  "By Town": {
+                      "items": [],
+                      "details": {
+                          "url": "/singapore-property/hdb-town/",
+                          "current_url": "https://ohmyhome.com/omh-sg/en-sg/singapore-property/hdb-directory",
+                          "is_published": true
+                      }
+                  }
+              }
+          ]
+      }
+  },
+  {
+      "Tools": {
+          "items": [
+              {
+                  "Homer Eligibility Checker": {
+                      "items": [],
+                      "details": {
+                          "url": "/homer/home-eligibility-checker",
+                          "current_url": null,
+                          "is_published": false
+                      }
+                  }
+              }
+          ]
+      }
+  },
+  {
+      "Ohmyhome": {
+          "items": [
+              {
+                  "About Us": {
+                      "items": [],
+                      "details": {
+                          "url": "/about",
+                          "current_url": null,
+                          "is_published": true
+                      }
+                  }
+              }
+          ]
+      }
+  }
+]
